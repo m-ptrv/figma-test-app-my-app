ae45875f199036f8293c8a055cd717273c1ff5aa
Maxim Petrov <>
2023-10-03 22:45:35 +0400

    Initial Commit

---
+++ src/views/DemoView.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Demo } from "@maxim-petrov/my-component-library";
+
+export const DemoView = () => (
+    <div>
+        <h1>This is your brand new application scaffold!</h1>
+    </div>
+);
