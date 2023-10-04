b334734dbfe42e2373daa0acf689e3d884ffd35b
Maxim Petrov <>
2023-10-04 22:10:37 +0400

    Update

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-import { Demo } from "@mptrv/my-component-library";
+//import { Demo } from "@mptrv/my-component-library";
 
 export const DemoView = () => (
     <div>
         <h1>This is my application!</h1>
-        <Demo />
     </div>
 );
