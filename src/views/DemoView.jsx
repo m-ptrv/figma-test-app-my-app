f84ffb14c3b1ba496d1795e52ca945162d2f51f2
Maxim Petrov <mail@maximpetrov.ru>
2023-12-09 20:04:03 +0400

    Turn on my npm package

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-//import { Demo } from "@mptrv/my-component-library";
+import { Demo } from "@mptrv/my-component-library";
 
 export const DemoView = () => (
     <div>
         <h1>This is my application!</h1>
+        <Demo />
     </div>
 );
